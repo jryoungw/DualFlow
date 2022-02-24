4ce1b95f144602952864e6038173ede955163ddc
jryoungw <et2035@naver.com>
2022-02-25 08:18:01 +0900

    Added

--- DualClass.py
+++ DualClass.py
@@ -8,6 +8,11 @@ class DualNumber():
             return DualNumber(self.real + other.real, self.dual + other.dual)
         else:
             return DualNumber(self.real + other, self.dual)
+    def __radd__(self, other):
+        if isinstance(other, DualNumber):
+            return DualNumber(self.real + other.real, self.dual + other.dual)
+        else:
+            return DualNumber(self.real + other, self.dual)
 
     def __sub__(self, other):
         if isinstance(other, DualNumber):
@@ -16,6 +21,11 @@ class DualNumber():
             return DualNumber(self.real - other, self.dual)
 
     def __mul__(self, other):
+        if isinstance(other, DualNumber):
+            return (self.real * other.real, self.real * other.dual + self.dual * other.real)
+        else:
+            return DualNumber(self.real * other, self.dual * other)
+    def __rmul__(self, other):
         if isinstance(other, DualNumber):
             return (self.real * other.real, self.real * other.dual + self.dual * other.real)
         else:
@@ -31,6 +41,12 @@ class DualNumber():
     def __pow__(self, other):
         assert isinstance(other, int)
         return DualNumber(self.real ** other, self.dual * other * self.real ** (other-1))
+    
+    def __neg__(self):
+        return DualNumber(-self.real, -self.dual)
+    
+    def __pos__(self):
+        return DualNumber(self.real, self.dual)
 
     def __repr__(self):
         if self.dual>=0:
--- Functions.py
+++ Functions.py
@@ -37,3 +37,23 @@ class Cos(DualNumber):
         for i in range(self.nth_expansion):
             cos_x += (self.dn ** (2*i)) * ((-1) ** i) / math.factorial(2*i)
         return cos_x
+    
+class Sigmoid(DualNumber):
+    def __init__(self, real, dual=1):
+        super().__init__(real, dual)
+        self.dn = DualNumber(real, dual)
+    def __call__(self):
+        """
+        1/(1+exp(-x)) cannot be implemented due to indivisibility of dual number system:
+            Dual number system have ring structure, not field structure.
+        Used Wolfram Alpha's Taylor expansion for sigmoid function
+        https://www.wolframalpha.com/input?i=taylor+series+of+sigmoid
+        """
+        sigmoid_x = 1/2
+        sigmoid_x = (self.dn)/4 + sigmoid_x
+        sigmoid_x = -1*((self.dn)**3)/48 + sigmoid_x
+        sigmoid_x = ((self.dn)**5)/480 + sigmoid_x
+        sigmoid_x = -1*((self.dn)**7)*17/80640 + sigmoid_x
+        sigmoid_x = ((self.dn)**9)*31/1452510 + sigmoid_x
+        sigmoid_x = -1*((self.dn)**11)*691/319334400 + sigmoid_x
+        return sigmoid_x
